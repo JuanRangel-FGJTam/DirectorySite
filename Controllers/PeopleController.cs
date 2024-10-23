0899b8f39408f6168cbc0f79aae1d26ae11a39c9
J. Salvador Rangel <juan.rangel@fgjtam.gob.mx>
2024-10-23 20:25:41 +0000

    Created view for display the person info.

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -42,7 +42,6 @@ namespace DirectorySite.Controllers
             try {
                 var peopleSearched = await this.peopleSearchService.SearchPerson(search!);
                 return View(peopleSearched);
-
             }catch(Exception err){
                 this._logger.LogError(err, "Fail at search the person");
                 ViewBag.ErrorMessage = "Error al realizar la busqueda, " + err.Message;
@@ -50,5 +49,36 @@ namespace DirectorySite.Controllers
             }
         }
 
+        [HttpGet]
+        [Route("{personID}")]
+        public async Task<IActionResult> Person([FromRoute] string personID){
+            
+            // * Prepare the request
+            var httpClient = httpClientFactory.CreateClient("DirectoryAPI");
+            var httpRequest = new HttpRequestMessage
+            {
+                RequestUri = new Uri(httpClient.BaseAddress!, $"/api/people/{personID}"),
+                Method = HttpMethod.Get
+            };
+            httpRequest.Headers.Add("Authorization", "Bearer " + HttpContext.Session.GetString("JWTToken"));
+            
+            
+            PersonResponse? personResponse = null;
+            try {
+                var httpResponse = await httpClient.SendAsync( httpRequest );
+                
+                // * Proccess the response
+                if( httpResponse.IsSuccessStatusCode ){
+                    var text = await httpResponse.Content.ReadAsStringAsync();
+                    this._logger.LogDebug("Response:[{response}]", text);
+                    personResponse = await httpResponse.Content.ReadFromJsonAsync<PersonResponse>();
+                }
+
+            }catch(Exception err){
+                this._logger.LogError(err, "Fail at get the data of the person '{personId}'", personID);
+            }
+
+            return View(personResponse);
+        }
     }
 }
