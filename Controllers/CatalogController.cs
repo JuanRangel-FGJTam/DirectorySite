78e55bc3f630f981a105983860d85dedf7e46080
Juan Salvador Rangel Almaguer <juan.rangel@fgjtam.gob.mx>
2025-06-03 13:58:55 -0600

    Implemented store catalogs.

--- Controllers/CatalogController.cs
+++ Controllers/CatalogController.cs
@@ -151,38 +151,64 @@ namespace DirectorySite.Controllers
             return View(viewModel);
         }
 
-        [HttpPost("States")]
-        public async Task<IActionResult> StoreState([FromForm] int countryId, [FromForm] string name)
+        [Route("states/new")]
+        public async Task<IActionResult> CreateState(int? countryId)
+        {
+            if(countryId != null && countryId > 0)
+            {
+                locaCountryID = countryId.Value;
+            }
+
+            var countries = (await catalogService.GetCountries()) ?? [];
+            var states = (await catalogService.GetStates(locaCountryID)) ?? [];
+            
+            var viewModel = new NewCatalogStatesViewModel
+            {
+                CountryId = locaCountryID,
+                Countries = countries.ToList(),
+                States = states.ToList()
+            };
+            ViewData["Title"] = "Catalogo - Registrar nuevo Estado";
+            ViewData["ActivePage"] = "Nuevo Estado";
+            return View("StateNew", viewModel);
+        }
+
+        [HttpPost("states/new")]
+        public async Task<IActionResult> StoreState(NewCatalogStatesViewModel request)
         {
             try
             {
                 // * get the country related
-                var countries = await catalogService.GetCountries() ?? throw new Exception("Errot at attempt to get the countries");
-                var country = countries.FirstOrDefault(item => item.Id == countryId) ?? throw new ArgumentException("The countryId was not found on the system", "countryId" );
+                var countries = await catalogService.GetCountries() ?? throw new Exception("Error al obtener el catalog de paises");
+                var country = countries.FirstOrDefault(item => item.Id == request.CountryId!) ?? throw new ArgumentException("El pais selecciondo no se encuentra en el sistema", "countryId");
 
                 // * validate the name
-                if(string.IsNullOrEmpty(name))
+                if (string.IsNullOrEmpty(request.StateName))
                 {
-                    throw new ArgumentException("The name must not be null and empty", "name" ); 
+                    throw new ArgumentException("The name must not be null and empty", "name");
                 }
 
-               // * register the new data
-               var res = await this.catalogService.StoreNewState(countryId, name);
-               
-                return Ok( new {
-                    Message = $"The state '{name}' was created"
-                });
+                // * register the new data
+                var res = await this.catalogService.StoreNewState(request.CountryId!.Value, request.StateName);
+
+                this._logger.LogInformation("The state '{stateName}' was created", request.StateName);
+                return RedirectToAction("States");
             }
-            catch(ArgumentException ae)
+            catch (ArgumentException ae)
             {
-                return UnprocessableEntity( new {
-                    Message = "Error al validar la solicitud",
-                    Errors = new Dictionary<string,string>{ { ae.ParamName??"err", ae.Message } }
-                });
+                var errors = new Dictionary<string, string> { { ae.ParamName ?? "err", ae.Message } };
+                ViewBag.ErrorMessage = string.Join("\n", errors.Values);
+
+                // * load again the catalogs
+                request.Countries = await catalogService.GetCountries() ?? [];
+
+                // * return the view
+                return View("StateNew", request);
             }
-            catch(Exception e)
+            catch (Exception e)
             {
-                return Conflict( new {
+                return Conflict(new
+                {
                     Title = "Error al registrar el estado",
                     Message = e.Message,
                 });
@@ -230,39 +256,68 @@ namespace DirectorySite.Controllers
             return View(viewModel);
         }
 
-        [HttpPost("Municipalities")]
-        public async Task<IActionResult> StoreMunicipality([FromForm] int countryId, [FromForm] int stateId, [FromForm] string name)
+        [Route("municipalities/new")]
+        public async Task<IActionResult> CreateMunicipality(int? countryId, int? stateId)
+        {
+            locaCountryID = countryId ?? 0;
+            localStateID = stateId ?? 0;
+
+            var countries = (await catalogService.GetCountries()) ?? [];
+            var states = (await catalogService.GetStates(locaCountryID)) ?? [];
+
+            // * override the stateId if is not in the catalog
+            if (!states.Select(item => item.Id).Contains(localStateID))
+            {
+                localStateID = states.FirstOrDefault()?.Id ?? -1;
+            }
+
+            // * return the view
+            var viewModel = new NewCatalogStatesViewModel
+            {
+                CountryId = locaCountryID,
+                StateId = localStateID,
+                Countries = countries.ToList(),
+                States = states.ToList()
+            };
+            ViewData["Title"] = "Catalogo - Registrar nuevo Municipio";
+            ViewData["ActivePage"] = "Nuevo Municipio";
+            return View("MunicipalitiesNew", viewModel);
+        }
+
+        [HttpPost("municipalities/new")]
+        public async Task<IActionResult> StoreMunicipality(NewCatalogStatesViewModel request)
         {
             try
             {
-                _logger.LogInformation("New countryId{id} sateId{sid} name{name}", countryId, stateId, name);
-
                 // * get the country related
-                var countries = await catalogService.GetCountries() ?? throw new Exception("Errot at attempt to get the countries");
-                var country = countries.FirstOrDefault(item => item.Id == countryId) ?? throw new ArgumentException("The countryId was not found on the system.", "countryId" );
+                var countries = await catalogService.GetCountries() ?? throw new Exception("Erro al obtener el catalogo de paises.");
+                var country = countries.FirstOrDefault(item => item.Id == request.CountryId) ?? throw new ArgumentException("El pais seleccionado no se encontro en el sistema.", "CountryId" );
                 
-                var states = await catalogService.GetStates(countryId) ?? throw new Exception("Errot at attempt to get the states");
-                var state = states.FirstOrDefault(item => item.Id == stateId) ?? throw new ArgumentException("The stateId was not found on the system.", "stateId" );
+                var states = await catalogService.GetStates(request.CountryId!.Value) ?? throw new Exception("No hay estados disponibles para el pais seleccionado.");
+                var state = states.FirstOrDefault(item => item.Id == request.StateId!.Value) ?? throw new ArgumentException("El estado seleccionado no se encuentra en el sistema.", "StateId" );
                 
                 // * validate the name
-                if(string.IsNullOrEmpty(name))
+                if(string.IsNullOrEmpty(request.MunicipalityName))
                 {
-                    throw new ArgumentException("The name must not be null and empty", "name" ); 
+                    throw new ArgumentException("El nombre del nuevo municipio es requerido.", "name" );
                 }
 
-               // * register the new catalog element
-               var res = await this.catalogService.StoreNewMunicipality(countryId, stateId, name);
+                // * register the new catalog element
+                var res = await this.catalogService.StoreNewMunicipality(request.CountryId!.Value, request.StateId!.Value, request.MunicipalityName);
 
-                return Ok( new {
-                    Message = $"The state '{name}' was created"
-                });
+                return RedirectToAction("Municipalities");
             }
             catch(ArgumentException ae)
             {
-                return UnprocessableEntity( new {
-                    Message = "Error al validar la solicitud",
-                    Errors = new Dictionary<string,string>{ { ae.ParamName??"err", ae.Message } }
-                });
+                var errors = new Dictionary<string, string> { { ae.ParamName ?? "err", ae.Message } };
+                ViewBag.ErrorMessage = string.Join("\n", errors.Values);
+
+                // * load again the catalogs
+                request.Countries = await catalogService.GetCountries() ?? [];
+                request.States = await catalogService.GetStates(request.CountryId!.Value) ?? [];
+
+                // * return the view
+                return View("MunicipalitiesNew", request);
             }
             catch(Exception e)
             {
@@ -323,40 +378,82 @@ namespace DirectorySite.Controllers
             return View(viewModel);
         }
         
-        [HttpPost("Colonies")]
-        public async Task<IActionResult> StoreColonies([FromForm] int countryId, [FromForm] int stateId, [FromForm] int municipalityId, [FromForm] string name, [FromForm] int zipCode)
+        [Route("colonies/new")]
+        public async Task<IActionResult> CreateColony(int? countryId, int? stateId, int? municipalityId)
+        {
+            locaCountryID = countryId ?? 138;
+            localStateID = stateId ?? 28;
+            localMunicipalityId = municipalityId ?? 41;
+
+            var countries = await catalogService.GetCountries() ?? [];
+
+            var states = await catalogService.GetStates(locaCountryID) ?? [];
+            // * override the stateId if is not in the catalog
+            if (!states.Select(item => item.Id).Contains(localStateID))
+            {
+                localStateID = states.FirstOrDefault()?.Id ?? -1;
+            }
+
+            var municipalities = await catalogService.GetMunicipalities(localStateID) ?? [];
+            if (!municipalities.Select(item => item.Id).Contains(localMunicipalityId))
+            {
+                localMunicipalityId = municipalities.FirstOrDefault()?.Id ?? -1;
+            }
+
+            // * return the view
+            var viewModel = new NewCatalogStatesViewModel
+            {
+                CountryId = locaCountryID,
+                StateId = localStateID,
+                MunicipalityId = localMunicipalityId,
+                Countries = countries.ToList(),
+                States = states.ToList(),
+                Municipalities = municipalities
+            };
+            ViewData["Title"] = "Catalogo - Registrar nueva Colonia";
+            ViewData["ActivePage"] = "Nueva Colonia";
+            return View("ColoniesNew", viewModel);
+        }
+
+        [HttpPost("colonies/new")]
+        public async Task<IActionResult> StoreColonies(NewCatalogStatesViewModel request)
         {
             try
             {
                 // * get the country related
-                var countries = await catalogService.GetCountries() ?? throw new Exception("Errot at attempt to get the countries");
-                var country = countries.FirstOrDefault(item => item.Id == countryId) ?? throw new ArgumentException("The countryId was not found on the system.", "countryId" );
+                var countries = await catalogService.GetCountries() ?? throw new Exception("Erro al obtener el catalogo de paises.");
+                var country = countries.FirstOrDefault(item => item.Id == request.CountryId) ?? throw new ArgumentException("El pais seleccionado no se encontro en el sistema.", "CountryId" );
                 
-                var states = await catalogService.GetStates(countryId) ?? throw new Exception("Errot at attempt to get the states");
-                var state = states.FirstOrDefault(item => item.Id == stateId) ?? throw new ArgumentException("The stateId was not found on the system.", "stateId" );
+                var states = await catalogService.GetStates(request.CountryId!.Value) ?? throw new Exception("No hay estados disponibles para el pais seleccionado.");
+                var state = states.FirstOrDefault(item => item.Id == request.StateId!.Value) ?? throw new ArgumentException("El estado seleccionado no se encuentra en el sistema.", "StateId" );
 
-                var municipalities = await catalogService.GetMunicipalities(stateId) ?? throw new Exception("Errot at attempt to get the municipalities");
-                var municipality = municipalities.FirstOrDefault(item => item.Id == municipalityId) ?? throw new ArgumentException("The municipalityId was not found on the system.", "municipalityId" );
+                var municipalities = await catalogService.GetMunicipalities(request.StateId!.Value) ?? throw new Exception("No hay municipios disponibles para el estado seleccionado");
+                var municipality = municipalities.FirstOrDefault(item => item.Id == request.MunicipalityId!.Value) ?? throw new ArgumentException("El municipio seleccionado no se encuentra en el sistema.", "municipalityId" );
                 
                 // * validate the name
-                if(string.IsNullOrEmpty(name))
+                if(string.IsNullOrEmpty(request.ColonyName))
                 {
-                    throw new ArgumentException("The name must not be null and empty", "name" ); 
+                    throw new ArgumentException("The nombre del la colonia es requerido", "name" );
                 }
 
-               // * register the new catalog element
-               var res = await this.catalogService.StoreNewColony(countryId, stateId, municipalityId, name, zipCode);
+                // * register the new catalog element
+                var res = await this.catalogService.StoreNewColony(country.Id!.Value, state.Id!.Value, municipality.Id!.Value, request.ColonyName, int.Parse(request.ZipCode ?? "0"));
 
-                return Ok( new {
-                    Message = $"The state '{name}' was created"
-                });
+                return RedirectToAction("Colonies");
             }
             catch(ArgumentException ae)
             {
-                return UnprocessableEntity( new {
-                    Message = "Error al validar la solicitud",
-                    Errors = new Dictionary<string,string>{ { ae.ParamName??"err", ae.Message } }
-                });
+                var errors = new Dictionary<string, string> { { ae.ParamName ?? "err", ae.Message } };
+                ViewBag.ErrorMessage = string.Join("\n", errors.Values);
+
+                // * load again the catalogs
+                request.Countries = await catalogService.GetCountries() ?? [];
+                request.States = await catalogService.GetStates(request.CountryId!.Value) ?? [];
+                var _stateID = request.States.FirstOrDefault()?.Id ?? -1;
+                request.Municipalities = await catalogService.GetMunicipalities(_stateID) ?? [];
+
+                // * return the view
+                return View("ColoniesNew", request);
             }
             catch(Exception e)
             {
